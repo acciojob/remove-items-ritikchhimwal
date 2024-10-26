287a701ddf25f02b8fb55c2443fb1e5558c1c26b
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-26 19:07:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', () => {
+    const colorSelect = document.getElementById("colorSelect");
+    const selectedIndex = colorSelect.selectedIndex;
+
+    // Remove the selected option if an item is selected
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+});
